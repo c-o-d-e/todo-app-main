99c50d6b17595cc6c367728d87aa112603b04594
c-o-d-e <swapyuk@gmail.com>
2022-08-14 00:14:31 +0100

    add taskCompleted toggle event

--- script.js
+++ script.js
@@ -3,9 +3,32 @@ function addTask() {
     document.getElementById("text-input").value = "";
     const taskContainer = document.getElementsByClassName("task-container");
     let newTask = `<div class="task">
-        <div class="circle"></div>
+        <div class="circle" onclick="taskCompleted()"></div>
         <h2 class="task-label">${textInput}</h2>
     </div>`;
     $(".task-container").append(newTask);
     console.log(newTask);
+}
+
+function taskCompleted() {
+    const domTokenList = event.target.classList;
+    console.log("user click:", event.target);
+
+    let tickMark = `<img
+        src="./images/icon-check.svg"
+        alt="task circle"
+    />`;
+
+    if (domTokenList.contains("completed")) {
+        if (event.target.firstChild) {
+            event.target.firstChild.remove();
+        }
+
+        event.target.classList.remove("completed");
+        event.target.nextElementSibling.classList.remove("strikethrough");
+    } else {
+        event.target.classList.add("completed");
+        event.target.nextElementSibling.classList.add("strikethrough");
+        $(event.target).append(tickMark);
+    }
 }
