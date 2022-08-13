82dcf2ed9cb3bbb01903565347c1ecbf06210fde
c-o-d-e <swapyuk@gmail.com>
2022-08-13 22:55:25 +0100

    change target container

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function addTask() {
     const textInput = document.getElementById("text-input").value;
     document.getElementById("text-input").value = "";
-    const allTodos = document.getElementsByClassName("all-todos");
+    const taskContainer = document.getElementsByClassName("task-container");
     let newTask = `<div class="task">
         <div class="circle"></div>
         <h2 class="task-label">${textInput}</h2>
     </div>`;
-    $(".all-todos").append(newTask);
+    $(".task-container").append(newTask);
     console.log(newTask);
 }
