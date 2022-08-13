a0cf56e452cc2b94d1cde5e4f5c36e97dd5cdedf
c-o-d-e <swapyuk@gmail.com>
2022-08-13 21:30:04 +0100

    addTask function first draft

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function addTask() {
     const textInput = document.getElementById("text-input").value;
     document.getElementById("text-input").value = "";
-    console.log(textInput);
+    const allTodos = document.getElementsByClassName("all-todos");
+    let newTask = `<div class="task">
+        <div class="circle"></div>
+        <h2 class="task-label">${textInput}</h2>
+    </div>`;
+    $(".all-todos").append(newTask);
+    console.log(newTask);
 }
