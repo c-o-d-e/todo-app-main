6789b41124c705db21dfebcfecd5985c147809c5
c-o-d-e <swapyuk@gmail.com>
2022-08-14 16:40:04 +0100

    tasks updating functions created

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let itemsLeft = Number(document.getElementById("items-left").innerHTML); //get number in the span of items left
+
 function addTask() {
     const textInput = document.getElementById("text-input").value;
     document.getElementById("text-input").value = "";
@@ -7,12 +9,12 @@ function addTask() {
         <h2 class="task-label">${textInput}</h2>
     </div>`;
     $(".task-container").append(newTask);
-    console.log(newTask);
+    itemsLeft++;
+    document.getElementById("items-left").innerHTML = itemsLeft; //updates html with correct itemsLeft
 }
 
 function taskCompleted(event) {
     const domTokenList = event.target.classList;
-    console.log("user click:", event.target);
 
     let tickMark = `<img
         src="./images/icon-check.svg"
@@ -26,9 +28,40 @@ function taskCompleted(event) {
 
         event.target.classList.remove("completed");
         event.target.nextElementSibling.classList.remove("strikethrough");
+        itemsLeft++;
     } else {
         event.target.classList.add("completed");
         event.target.nextElementSibling.classList.add("strikethrough");
         $(event.target).append(tickMark);
+        itemsLeft--;
+    }
+    document.getElementById("items-left").innerHTML = itemsLeft; //updates html with correct itemsLeft
+}
+
+const task = document.getElementsByClassName("task"); //gets an object with all task divs
+
+function activeTasks() {
+    for (i = 0; i < task.length; i++) {
+        if (task[i].firstElementChild.classList.contains("completed")) {
+            task[i].style.display = "none";
+        } else {
+            task[i].style.display = "flex";
+        }
+    }
+}
+
+function completedTasks() {
+    for (i = 0; i < task.length; i++) {
+        if (task[i].firstElementChild.classList.contains("completed")) {
+            task[i].style.display = "flex";
+        } else {
+            task[i].style.display = "none";
+        }
+    }
+}
+
+function allTasks() {
+    for (i = 0; i < task.length; i++) {
+        task[i].style.display = "flex";
     }
 }
